7fce2c8caeb8cf4b4141fe0f17b3f48c64ceaa4d
majidln <majidlotfinia12@gmail.com>
2020-09-22 16:30:51 +0330

    install apollo client

--- src/services/apollo.js
+++ src/services/apollo.js
@@ -1,16 +1,11 @@
-import fetch from 'node-fetch';
-import {createHttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
-import ApolloClient from 'apollo-client';
+import {ApolloClient, InMemoryCache} from '@apollo/client';
 
 const API_URL = 'https://swapi-graphql.netlify.com/.netlify/functions/index';
 
+const cache = new InMemoryCache();
 const client = new ApolloClient({
-  link: createHttpLink({
-    uri: API_URL,
-    fetch: fetch,
-  }),
-  cache: new InMemoryCache(),
+  uri: API_URL,
+  cache,
 });
 
 export {client};
