daa5f5da7670c020346d2e0282438d7132e36e0d
majidln <majidlotfinia12@gmail.com>
2020-09-30 23:43:43 +0330

    remove viewable item change event

--- src/screen/list.js
+++ src/screen/list.js
@@ -51,11 +51,6 @@ export default function list() {
     return <Text>No data!</Text>;
   }
 
-  const onViewableChanged = ({viewableItems, changed}) => {
-    console.log('Visible items are', viewableItems);
-    console.log('Changed in this iteration', changed);
-  };
-
   const renderItem = ({item, index}) => {
     return (
       <View style={styles.itemWrapper}>
@@ -72,10 +67,6 @@ export default function list() {
         renderItem={renderItem}
         keyExtractor={item => item.node.id}
         horizontal={true}
-        onViewableItemsChanged={onViewableChanged}
-        viewabilityConfig={{
-          itemVisiblePercentThreshold: 80,
-        }}
       />
     </SafeAreaView>
   );
