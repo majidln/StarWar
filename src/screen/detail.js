f901b5a8493cbf986e8a1b37a3de9e26978f302a
majidln <majidlotfinia12@gmail.com>
2020-10-05 18:13:47 +0330

    add image on detail screen

--- src/screen/detail.js
+++ src/screen/detail.js
@@ -1,10 +1,39 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {Image, View, Text, StyleSheet, Dimensions} from 'react-native';
+import {Container} from '@common-component';
 
-export default function Detail() {
+const {height} = Dimensions.get('window');
+const POSTER_HEIGHT = height * 0.68;
+
+export default function Detail({route}) {
+  const {movie} = route.params;
   return (
-    <View>
-      <Text>In Detail</Text>
-    </View>
+    <Container>
+      <View style={styles.wrapper}>
+        <Image source={movie.poster} style={styles.poster} />
+        <Text style={styles.title}>{movie.node.title}</Text>
+      </View>
+    </Container>
   );
 }
+
+const styles = StyleSheet.create({
+  wrapper: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    flexDirection: 'column',
+    backgroundColor: 'red',
+    height: POSTER_HEIGHT,
+  },
+  poster: {
+    height: POSTER_HEIGHT,
+    width: '100%',
+    resizeMode: 'cover',
+    position: 'absolute',
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+});
