65a539ac5b508896ffa1d4a18dea36b50c8a96a5
majidln <majidlotfinia12@gmail.com>
2020-10-16 22:58:57 +0330

    add styles to rank component

--- src/components/common/rank.js
+++ src/components/common/rank.js
@@ -1,10 +1,24 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, StyleSheet} from 'react-native';
 
-export function Rank({rank = 2}) {
+export function Rank({rank = 4}) {
   return (
     <View>
-      <Text>{rank}</Text>
+      <Text style={styles.text}>
+        Rank is: <Text style={styles.rank}>{rank}</Text>
+      </Text>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    color: 'gray',
+  },
+  rank: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: '900',
+  },
+});
